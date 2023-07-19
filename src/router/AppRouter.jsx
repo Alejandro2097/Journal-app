11eba7220bebd6a7725c1df9428ec21730cc622d
Alejandro2097 <alejo12381@hotmail.es>
2023-07-18 19:30:17 -0500

    react router dom

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        {/*  Login y registro */}
+        <Route/>
+        {/*  JournalApp */}
+    </Routes>
+  )
+}
