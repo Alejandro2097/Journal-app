e8e4d620963573a0d11fb708f452d24717aaf4e2
Alejandro2097 <alejo12381@hotmail.es>
2023-07-22 23:40:26 -0500

    default route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage, RegisterPage } from '../pages';
 
 export const AppRouter = () => {
   return (
     <Routes>
-        {/*  Login y registro */}
         <Route path="login" element={<LoginPage/>}/>
-        {/*  JournalApp */}
         <Route path="register" element={<RegisterPage/>}/>
+        <Route path="/*" element={<Navigate to="/auth/login"/>}/>
     </Routes>
   )
 }
