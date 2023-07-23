5e0b9969868281c01758d16d5bd6e3d0ce48a4af
Alejandro2097 <alejo12381@hotmail.es>
2023-07-22 23:55:23 -0500

    otras rtutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../pages';
+
+import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
+
 
 export const AppRouter = () => {
   return (
     <Routes>
-        {/*  Login y registro */}
-        <Route path="login" element={<LoginPage/>}/>
-        {/*  JournalApp */}
-        <Route/>
+
+        {/* Login y Registro */}
+        <Route path="/auth/*" element={ <AuthRoutes /> } />
+
+        {/* JournalApp */}
+        <Route path="/*" element={ <JournalRoutes /> } />
+
     </Routes>
   )
 }
