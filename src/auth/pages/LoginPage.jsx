8601d6ac8e2bc2ccee865bf754106a8e154e2bc2
Alejandro2097 <alejo12381@hotmail.es>
2023-07-23 20:03:32 -0500

    styling

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material"
+import { Grid, Typography, TextField, Button } from "@mui/material"
 
 
 export const LoginPage = () => {
@@ -17,6 +17,39 @@ export const LoginPage = () => {
         sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2}}
       >
         <Typography variant='h5' sx={{mb: 1}}>Login</Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder="correo@google.com"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField 
+                label="Password" 
+                type="password" 
+                placeholder="Password"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
       </Grid>
 
     </Grid>
