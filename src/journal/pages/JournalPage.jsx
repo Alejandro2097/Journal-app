aa003a2b02c7db43df078b1ec526ba3eb1db576b
Alejandro2097 <alejo12381@hotmail.es>
2023-07-25 22:05:22 -0500

    working

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,8 @@
-// import { AddOutlined } from '../views';
 import { AddOutlined } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -10,8 +10,8 @@ export const JournalPage = () => {
       
       {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      {/* <NoteView /> */}
+      <NothingSelectedView />
+      <NoteView /> 
 
 
       <IconButton
