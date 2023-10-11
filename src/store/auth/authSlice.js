bf2977bfaab75644c51003a615ecab5dcec1ce44
Alejandro2097 <alejo12381@hotmail.es>
2023-10-11 18:52:26 -0500

    slice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  //nombre del slice con el que se vincula al store
+  name: 'auth',
+  //declaracion del estado inicial de las variables
+  initialState: {
+    status: 'cheking', // 'not-authenticated', 'authenticated'
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null
+  },
+  //funciones que modifican el estado
+  reducers: {
+    login: (state, action) => {
+
+    },
+    logout: (state, payload) => { 
+
+    },
+    checkingCredentials: (state) => {
+
+    }
+  },
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
