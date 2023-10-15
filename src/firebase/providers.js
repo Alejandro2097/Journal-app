99e75635211547c9f17d9a547e514a5a405c0a19
Alejandro2097 <alejo12381@hotmail.es>
2023-10-14 23:09:21 -0500

    firebaseauth

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,32 +1,75 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { FirebaseAuth } from "./config";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 
+import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
 
+export const singInWithGoogle = async() => {
 
-export const signInWithGoogle = async() => { 
-
-
-    try{
-
-        const result = await signInWithPopup(FirebaseAuth, googleProvider);
-        // const credentials = GoogleAuthProvider.credentialFromResult(result);
+    try {
+        
+        const result = await signInWithPopup(FirebaseAuth, googleProvider );
+        // const credentials = GoogleAuthProvider.credentialFromResult( result );
         const { displayName, email, photoURL, uid } = result.user;
         
         return {
             ok: true,
-            // User Info
+            // User info
             displayName, email, photoURL, uid
         }
-    }catch(error) {
+        
+
+    } catch (error) {
+        
         const errorCode = error.code;
         const errorMessage = error.message;
-
+    
         return {
             ok: false,
-            errorCode,
-            errorMessage
+            errorMessage,
         }
     }
+
+}
+
+
+export const registerUserWithEmailPassword = async({ email, password, displayName }) => {
+
+    try {
+        const resp = await createUserWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL } = resp.user;
+
+        await updateProfile( FirebaseAuth.currentUser, { displayName });
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+
+    } catch (error) {
+        console.log(error);
+        return { ok: false, errorMessage: error.message }
+    }
+
+}
+
+
+export const loginWithEmailPassword = async({ email, password }) => {
+
+    try {
+        const resp = await signInWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL, displayName } = resp.user;
+
+        return {
+            ok: true,
+            uid, photoURL, displayName
+        }
+
+    } catch (error) {
+        return { ok: false, errorMessage: error.message }
+    }
+}
+
+export const logoutFirebase = async() => {
+    return await FirebaseAuth.signOut();
 }
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,21 +1,65 @@
-import { signInWithGoogle } from "../../firebase/providers"
-import { checkingCredentials, login, logout } from "./authSlice"
+import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, singInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, login, logout } from './';
 
-export const checkingAuthentication = (email, password) => { 
-    return async(dispatch) => { 
+export const checkingAuthentication = () => {
+    return async( dispatch ) => {
 
-        dispatch(checkingCredentials())
+        dispatch( checkingCredentials() );
+        
     }
 }
 
 
-export const startGoogleSignIn = () => { 
-    return async( dispatch) => { 
-        dispatch(checkingCredentials());
+export const startGoogleSignIn = () => {
+    return async( dispatch ) => {
 
-        const result = await  signInWithGoogle();
+        dispatch( checkingCredentials() );
+
+        const result = await singInWithGoogle();
+        if ( !result.ok ) return dispatch( logout( result.errorMessage ) );
+
+        dispatch( login( result ))
+
+    }
+}
+
+
+export const startCreatingUserWithEmailPassword = ({ email, password, displayName }) => {
+    return async( dispatch ) => {
+
+        dispatch( checkingCredentials() );
+
+        const result = await registerUserWithEmailPassword({ email, password, displayName });
+        if ( !result.ok ) return dispatch( logout( result.errorMessage ) );
+
+        dispatch( login( result ))
+
+    }
+
+}
+
+
+export const startLoginWithEmailPassword = ({ email, password }) => {
+    return async( dispatch ) => {
+
+        dispatch( checkingCredentials() );
+
+        const result = await loginWithEmailPassword({ email, password });
+        console.log(result);
+
+        if ( !result.ok ) return dispatch( logout( result ) );
+        dispatch( login( result ));
+
+    }
+}
+
+
+export const startLogout = () => {
+    return async( dispatch ) => {
         
-        if(!result.ok) dispatch(logout(result.errorMessage));
-        dispatch(login(result))
+        await logoutFirebase();
+
+        dispatch( logout() );
+
     }
 }
