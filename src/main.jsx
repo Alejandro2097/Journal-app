e99957ed1174172e439464a6197e20188d1e8cf8
Alejandro2097 <alejo12381@hotmail.es>
2023-10-14 23:59:12 -0500

    secciom

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-import { JournalApp } from './JournalApp.jsx'
-
-import './styles.css'
-import { store } from './store'
+import { JournalApp } from './JournalApp';
+import { store } from './store';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter >
+    <Provider store={ store }>
+      <BrowserRouter>
         <JournalApp />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
