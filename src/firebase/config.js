675f542ba2859e12f2b5c61c833dc723e22baa94
Alejandro2097 <alejo12381@hotmail.es>
2023-10-15 19:34:20 -0500

    test y final de la seccion

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 
@@ -15,10 +15,7 @@ const firebaseConfig = {
   messagingSenderId: "223865835213",
   appId: "1:223865835213:web:690fc11f3577c68e712384"
 };
-
 // Initialize Firebase
-export const FirebaseApp = initializeApp(firebaseConfig);
-
-export const FirebaseAuth = getAuth(FirebaseApp);
-
-export const FirestoreDB = getFirestore(FirebaseApp);
+export const FirebaseApp  = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth( FirebaseApp );
+export const FirebaseDB   = getFirestore( FirebaseApp );
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -1,43 +1,81 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const journalSlice = createSlice({
-  //nombre del slice con el que se vincula al store
-  name: 'journal',
-  //declaracion del estado inicial de las variables
-  initialState: {
-    isSaving: true,
-    messageSave: '',
-    notes: [],
-    active: null
-    // active: {
-    //     id: '123',
-    //     title: '',
-    //     body: '',
-    //     date: 123456,
-    //     imageUrls: []
-    // }
-  },
-  //funciones que modifican el estado
-  reducers: {
-    addNewEmptyNote: (state, action) => { 
-
+    name: 'journal',
+    initialState: {
+        isSaving: false,
+        messageSaved: '',
+        notes: [],
+        active: null,
+        // active: {
+        //     id: 'ABC123',
+        //     title: '',
+        //     body: '',
+        //     date: 1234567,
+        //     imageUrls: [], // https://foto1.jpg, https://foto2.jpg, https://foto3.jpg
+        // }
     },
-    setActiveNote: (state, action) => { 
+    reducers: {
+        savingNewNote: ( state ) => {
+            state.isSaving = true;
+        },
+        addNewEmptyNote: (state, action ) => {
+            state.notes.push( action.payload );
+            state.isSaving = false;
+        },
+        setActiveNote: (state, action ) => {
+            state.active = action.payload;
+            state.messageSaved = '';
+        },
+        setNotes: (state, action ) => {
+            state.notes = action.payload;
+        },
+        setSaving: (state ) => {
+            state.isSaving = true;
+            state.messageSaved = '';
+        },
+        updateNote: (state, action ) => { // payload: note
+            state.isSaving = false;
+            state.notes = state.notes.map( note => {
 
-    },
-    setNotes: (state, action) => {
+                if ( note.id === action.payload.id ) {
+                    return action.payload;
+                }
 
-    },
-    setSaving: (state ) => { 
+                return note;
+            });
 
-    },
-    updateNote: (state, action) =>{ 
+            state.messageSaved = `${ action.payload.title }, actualizada correctamente`;
+        },
+        setPhotosToActiveNote: (state, action) => {
+            state.active.imageUrls = [ ...state.active.imageUrls, ...action.payload ]; 
+            state.isSaving = false;
+        },
 
-    },
-    deleteNoteById: (state, action) => {
-        
+        clearNotesLogout: (state) => {
+            state.isSaving = false;
+            state.messageSaved = '';
+            state.notes = [];
+            state.active = null;
+        },
+
+        deleteNoteById: (state, action ) => {
+            state.active = null;
+            state.notes = state.notes.filter( note => note.id !== action.payload );
+        },
     }
-  },
 });
 
-export const { increment } = journalSlice.actions;
+
+// Action creators are generated for each case reducer function
+export const { 
+    addNewEmptyNote,
+    clearNotesLogout,
+    deleteNoteById, 
+    savingNewNote,
+    setActiveNote,
+    setNotes,
+    setPhotosToActiveNote,
+    setSaving,
+    updateNote,
+} = journalSlice.actions;
